e95e10ddc1b81418b5f682272653f38449a2b3fc
AipuAmeh <aipuameh.16@gmail.com>
2024-01-29 15:29:21 -0500

    styled list points

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -9,7 +9,13 @@ const styles = {
     backgroundColor: "#F8F3EB",
   },
   brandName: {
-    fontSize: "100px"
+    fontSize: "100px",
+    textShadow: "3px 3px #C49A6C"
+  }, 
+  listStyles: {
+    listStyleType: "square",
+    // width: "2em",
+    // backgroundColor: "#C49A6C"
   }
 };
 
@@ -22,12 +28,7 @@ const Home = () => {
         alt="psychquiconsult-brand"
         className="transparent-logo max-w-xs p-8">
         </img>
-        <h1 className="font-serif " style={styles.brandName}>PSYCHQUICONSULT</h1>
-        {/* <img
-          src="images/logo-transparent.png"
-          alt="psychquiconsult-brand"
-          className="transparent-logo max-w-4xl p-8"
-        ></img> */}
+        <h1 className="font-serif tracking-wide" style={styles.brandName}>PSYCHQUICONSULT</h1>
       </div>
       <span className="flex justify-center text-xl py-4">
         Bridging the Gap In Psychatric Managment Through Consultation and
@@ -37,7 +38,7 @@ const Home = () => {
         style={styles.loginSection}
         className="login-section flex justify-center grid py-4"
       >
-        <h2 className="flex align-center text-xl p-4">
+        <h2 className="flex align-center text-2xl p-4">
           New Here? Login or Sign up
         </h2>
         <div className="button-styles flex flex-row p-6 justify-between">
@@ -56,12 +57,12 @@ const Home = () => {
         </div>
       </div>
       <section className="py-12">
-        <h1 className="flex justify-center text-xl mb-3">Why We Care</h1>
+        <h1 className="flex justify-center text-2xl mb-3">Why We Care</h1>
         <div className="flex flex-col px-4">
-          <ol className="flex flex-col align-baseline list-decimal leading-10 mx-auto text-xl">
-            <li>In 2021, 22.8% of US adults experienced mental illness, and 5.5% experienced serious mental illness. (NIH)</li>
-            <li>The presence of Serious Mental Illness (SMI) was higher among females than males. (NIH)</li>
-            <li>Young adults aged 18-25 had the highest prevalence of SMI compared to adults 26-49. (NIH)</li>
+          <ol className="flex flex-col align-baseline list-decimal leading-10 mx-auto text-xl" style={styles.listStyles}>
+            <li className="fact">In 2021, 22.8% of US adults experienced mental illness, and 5.5% experienced serious mental illness. (NIH)</li>
+            <li className="fact">The presence of Serious Mental Illness (SMI) was higher among females than males. (NIH)</li>
+            <li className="fact">Young adults aged 18-25 had the highest prevalence of SMI compared to adults 26-49. (NIH)</li>
           </ol>
         </div>
       </section>
